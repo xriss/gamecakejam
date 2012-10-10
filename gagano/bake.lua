1b4d51b2629e1bac4f026ed2bd7fc58de89c34af
kriss <devnull@localhost>
2012-10-10 21:28:36 +0100

    .

---
+++ gagano/bake.lua
@@ -0,0 +1,28 @@
+#!../../bin/dbg/lua
+
+local wbake=require("wetgenes.bake")
+local wstr=require("wetgenes.string")
+local wgrd=require("wetgenes.grd")
+local wgrdmap=require("wetgenes.grdmap")
+
+local lfs=require("lfs")
+local zip=require("zip")
+
+
+for _,dir in ipairs{"imgs"} do
+
+	local files=wbake.findfiles{basedir="art",dir=dir,filter="."}.ret
+
+	for i,v in ipairs(files) do
+		wbake.create_dir_for_file("data/"..v)
+		wbake.copyfile("art/"..v,"data/"..v)
+		print(v)
+	end
+
+end
+
+
+wbake.create_dir_for_file("out/gagano.zip")
+os.execute("rm out/gagano.zip")
+os.execute("zip -r out/gagano.zip data lua opts.lua")
+
