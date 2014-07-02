8ea8581ac43975b7e0e1ecd4e7b8f315617da484
kriss <devnull@localhost>
2014-07-02 18:34:21 +0100

    ,

---
+++ depart/lua/depart/serv.lua
@@ -0,0 +1,27 @@
+-- copy all globals into locals, some locals are prefixed with a G to reduce name clashes
+local coroutine,package,string,table,math,io,os,debug,assert,dofile,error,_G,getfenv,getmetatable,ipairs,Gload,loadfile,loadstring,next,pairs,pcall,print,rawequal,rawget,rawset,select,setfenv,setmetatable,tonumber,tostring,type,unpack,_VERSION,xpcall,module,require=coroutine,package,string,table,math,io,os,debug,assert,dofile,error,_G,getfenv,getmetatable,ipairs,load,loadfile,loadstring,next,pairs,pcall,print,rawequal,rawget,rawset,select,setfenv,setmetatable,tonumber,tostring,type,unpack,_VERSION,xpcall,module,require
+
+local pack=require("wetgenes.pack")
+local wwin=require("wetgenes.win")
+local wstr=require("wetgenes.string")
+local tardis=require("wetgenes.tardis")	-- matrix/vector math
+
+--local lanes=require("lanes")
+--if lanes.configure then lanes.configure() end -- depends on version
+
+--module
+local M={ modname=(...) } ; package.loaded[M.modname]=M
+
+M.bake=function(oven,serv)
+	serv=serv or {}
+	serv.modname=M.modname
+	
+	serv.setup=function(opts)
+	
+	
+	
+	end	
+	
+	return serv
+end
+
