9bb1daea3e2b9e7d3c2e98d631846981526dd93d
kriss <devnull@localhost>
2014-07-03 14:33:49 +0100

    html

---
+++ depart/html/depart.js
@@ -0,0 +1,21 @@
+
+
+$( document ).ready(function() {
+
+var url="/depart"; // use base domain we loaded from but change port?
+
+var data='{cmd="test"}';
+
+var cb=function(suc,dat)
+{
+};
+
+$.ajax({
+  url: url,
+  data: data,
+  success: cb,
+  dataType:"json"
+});
+
+
+});
