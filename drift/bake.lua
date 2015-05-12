67ef428ec118f2ff8cfaef638f73118d1ea8f95d
kriss <devnull@localhost>
2015-05-12 20:30:28 +0100

    .

--- drift/bake.lua
+++ drift/bake.lua
@@ -56,8 +56,8 @@ end
 
 os.execute("rm -rf out")
 wbake.create_dir_for_file("out/lua/wetgenes/t.zip")
-os.execute("zip -r out/nudgel.zip data lua opts.lua")
+os.execute("zip -r out/drift.zip data lua opts.lua")
 
 -- include snapshot of base modules for version safeness, probably.
 os.execute("cp -r ../../bin/lua/wetgenes out/lua/")
-os.execute("cd out ; zip -r nudgel.zip lua")
+os.execute("cd out ; zip -r drift.zip lua")
